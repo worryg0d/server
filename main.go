5a9b82399cdc9900290b16e17dda5b99450d4610
worryg0d <87984465+worryg0d@users.noreply.github.com>
2021-10-29 13:05:51 +0300

    func HandleFuncFADE
    HandleFunc for all dirs entries.

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func PrintFile(w http.ResponseWriter, req *http.Request) {
+	path := "." + req.URL.RequestURI()
+	data, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Fprint(w, err.Error())
+		return
+	}
+	fmt.Fprint(w, string(data))
+}
+
+// HandleFunc For All Dirs Entries
+func HandleFuncFADE(path string, f func(http.ResponseWriter, *http.Request)) {
+	dirEnrtry, err := os.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+	for _, val := range dirEnrtry {
+		if val.IsDir() {
+			HandleFuncFADE(path+"/"+val.Name(), f)
+		}
+		http.HandleFunc(path[1:]+"/"+val.Name(), f)
+	}
+}
+
+func main() {
+	HandleFuncFADE(".", PrintFile)
+	http.ListenAndServe(":8080", nil)
+}
